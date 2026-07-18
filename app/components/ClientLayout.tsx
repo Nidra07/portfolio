40633ea8943cf88bca233b20efe2fa641aaedfa2
Nidra07 <161415110+Nidra07@users.noreply.github.com>
2026-07-19 01:36:22 +0530

    Create ClientLayout.tsx

---
+++ app/components/ClientLayout.tsx
@@ -0,0 +1,46 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import { AnimatePresence, motion } from "framer-motion";
+import LoadingScreen from "./LoadingScreen";
+
+export default function ClientLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const loaded = sessionStorage.getItem("portfolio-loaded");
+
+    if (loaded) {
+      setLoading(false);
+    }
+  }, []);
+
+  const finishLoading = () => {
+    sessionStorage.setItem("portfolio-loaded", "true");
+    setLoading(false);
+  };
+
+  return (
+    <AnimatePresence mode="wait">
+      {loading ? (
+        <LoadingScreen key="loader" onComplete={finishLoading} />
+      ) : (
+        <motion.div
+          key="content"
+          initial={{ opacity: 0, y: 25 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{
+            duration: 0.8,
+            ease: "easeOut",
+          }}
+        >
+          {children}
+        </motion.div>
+      )}
+    </AnimatePresence>
+  );
+}
