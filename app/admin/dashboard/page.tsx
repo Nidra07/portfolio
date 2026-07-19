40e03600636ca42223422ec340d1e6cb018e9ff6
Nidra07 <161415110+Nidra07@users.noreply.github.com>
2026-07-19 14:37:19 +0530

    Update page.tsx

--- app/admin/dashboard/page.tsx
+++ app/admin/dashboard/page.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 export default function DashboardPage() {
   const cards = [
     "Hero",
@@ -16,7 +17,18 @@ export default function DashboardPage() {
       </h1>
 
       <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-3">
-        {cards.map((card) => (
+        {card === "Hero" ? (
+  <Link
+    href="/admin/dashboard/hero"
+    className="mt-6 inline-block rounded-lg bg-cyan-500 px-4 py-2 font-semibold text-black"
+  >
+    Open
+  </Link>
+) : (
+  <button className="mt-6 rounded-lg bg-cyan-500 px-4 py-2 font-semibold text-black">
+    Open
+  </button>
+)}
           <div
             key={card}
             className="rounded-2xl border border-white/10 bg-white/5 p-6 backdrop-blur-xl transition hover:border-cyan-400 hover:bg-white/10"
